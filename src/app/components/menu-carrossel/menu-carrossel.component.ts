e089b840c0e9861fa9fe59304fabeebd61bd37de
Leo <leonardosilva01107@gmail.com>
2023-11-13 17:08:10 -0300

    add carrossel na home

--- src/app/components/menu-carrossel/menu-carrossel.component.ts
+++ src/app/components/menu-carrossel/menu-carrossel.component.ts
@@ -1,10 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { CardModel } from 'src/app/models/cardModel';
+
+import KeenSlider, { KeenSliderInstance } from "keen-slider"
 
 @Component({
   selector: 'app-menu-carrossel',
   templateUrl: './menu-carrossel.component.html',
-  styleUrls: ['./menu-carrossel.component.css']
+  styleUrls: [
+     "../../../../node_modules/keen-slider/keen-slider.min.css",
+    './menu-carrossel.component.css']
 })
 export class MenuCarrosselComponent {
 
@@ -12,7 +16,32 @@ export class MenuCarrosselComponent {
   title:string =''
 
   @Input()
-  cards: CardModel[] =[
-  ] 
+  cards: CardModel[] =[] 
+
+
+
+  @ViewChild("sliderRef")
+  sliderRef!: ElementRef<HTMLElement>;
+
+  slider: any = null
+
+ 
+
+  ngAfterViewInit() {
+    this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+      loop: true,
+      rtl: false,
+      slides: {
+        perView: 4,
+        spacing: 1,
+      },
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.slider) this.slider.destroy()
+  }
+
+
 
 }
--- src/app/dataFake/data-card.ts
+++ src/app/dataFake/data-card.ts
@@ -6,9 +6,11 @@ export const dataFakeCard = {
             "id": 1,
             "title":"Design",
             "cards":[
-                {"id":1,"icon":"https://cdn-icons-png.flaticon.com/512/69/69544.png", "title": "Mudar cor de Imagem","description": "Você pode facilmente retirar o plano de fundo indesejado e obter imagens com transparência." },
-                {"id":1,"icon":"https://cdn-icons-png.flaticon.com/512/69/69544.png", "title": "Mudar cor de Imagem","description": "Você pode facilmente retirar o plano de fundo indesejado e obter imagens com transparência." },
-                
+                {"id":1,"icon":"", "title": "Mudar cor de Imagem","description": "Você pode facilmente retirar o plano de fundo indesejado e obter imagens com transparência." },
+                {"id":1,"icon":"", "title": "Mudar cor de Imagem","description": "Você pode facilmente retirar o plano de fundo indesejado e obter imagens com transparência." },
+                {"id":1,"icon":"", "title": "Mudar cor de Imagem","description": "Você pode facilmente retirar o plano de fundo indesejado e obter imagens com transparência." },
+                {"id":1,"icon":"", "title": "Mudar cor de Imagem","description": "Você pode facilmente retirar o plano de fundo indesejado e obter imagens com transparência." },
+                {"id":1,"icon":"", "title": "Mudar cor de Imagem","description": "Você pode facilmente retirar o plano de fundo indesejado e obter imagens com transparência." },
             ]
         },
 
@@ -18,7 +20,9 @@ export const dataFakeCard = {
             "cards":[
                 {"id":1,"icon":"https://cdn-icons-png.flaticon.com/512/69/69544.png", "title": "teste","description": "teste." },
                 {"id":1,"icon":"https://cdn-icons-png.flaticon.com/512/69/69544.png", "title": "Mudar cor de Imagem","description": "Você pode facilmente retirar o plano de fundo indesejado e obter imagens com transparência." },
-                
+                {"id":1,"icon":"https://cdn-icons-png.flaticon.com/512/69/69544.png", "title": "Mudar cor de Imagem","description": "Você pode facilmente retirar o plano de fundo indesejado e obter imagens com transparência." },
+                {"id":1,"icon":"https://cdn-icons-png.flaticon.com/512/69/69544.png", "title": "Mudar cor de Imagem","description": "Você pode facilmente retirar o plano de fundo indesejado e obter imagens com transparência." },
+                {"id":1,"icon":"https://cdn-icons-png.flaticon.com/512/69/69544.png", "title": "Mudar cor de Imagem","description": "Você pode facilmente retirar o plano de fundo indesejado e obter imagens com transparência." },
             ]
         },
 
