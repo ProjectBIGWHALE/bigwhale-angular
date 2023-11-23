626b35c5d02b37232207cfab1e39baec595a7513
Kleby <kleby10@gmail.com>
2023-11-23 20:06:29 -0300

    feat: Adicionado components de input file e text

---
+++ src/app/components/utils/input-file/input-file.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-file',
+  templateUrl: './input-file.component.html',
+  styleUrls: ['./input-file.component.css']
+})
+export class InputFileComponent {
+
+  @Input() controlName!: FormControl | any;
+  @Input() filelabelTitle: string = '';
+  @Input() buttonTitle : string ='';
+  @Input() errorMessage: string = 'O campo é obrigatório';
+
+  @Input() isSelected: boolean = false;
+
+  @Output() fileContent = new EventEmitter<any>();
+
+  fileName: string = ''
+
+  onChangeFile(target:any){  
+    const selectedFile: File = target.srcElement.files[0];
+    this.fileName = selectedFile.name;    
+    return this.fileContent.emit(selectedFile);
+  }
+
+  onClick(): any{    
+    return document.getElementById("selectedFile")?.click(); 
+  }
+}
