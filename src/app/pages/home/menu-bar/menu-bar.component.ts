929aaa1f3cca82a3edb2dab02a3f9d2619ea3501
Leo <leonardosilva01107@gmail.com>
2023-12-01 21:07:05 -0300

    ajustes nos botoes de tradução

--- src/app/pages/home/menu-bar/menu-bar.component.ts
+++ src/app/pages/home/menu-bar/menu-bar.component.ts
@@ -7,7 +7,37 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./menu-bar.component.css'],
 })
 export class MenuBarComponent {
-  constructor(private translate: TranslateService) {
 
+
+
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('pt-BR');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang?.match(/pt-BR|en/) ? browserLang : 'pt-BR');
   }
+
+  // Função para alternar o idioma para inglês
+  switchToEnglish() {
+    this.translate.use('en');
+  }
+
+  // Função para alternar o idioma para português
+  switchToPortuguese() {
+    this.translate.use('pt-BR');
+  }
+  switchToEspanol() {
+    this.translate.use('es');
+  }
+
+
+
+
+
+
+
+
+
+
 }
