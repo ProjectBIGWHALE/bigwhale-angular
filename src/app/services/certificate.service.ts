b74822c091863c725787b788772ea06b5c00f1cd
Erasmo Bezerra <erasmo.ads.tech@gmail.com>
2024-02-26 15:02:10 -0300

    fix: removed bugs in CertificateService

--- src/app/services/certificate.service.ts
+++ src/app/services/certificate.service.ts
@@ -16,22 +16,14 @@ export class CertificateService{
 
     constructor(private http: HttpClient){}
 
-    saveCertificate(form: Certificate):Observable<Certificate>{
-        //iniciar o loading 
+    saveCertificate(form: Certificate):Observable<Certificate>{                     
 
-        this.loadingSubject.next(true);
-
-        form.dateIssue = this.getDateIssue();        
-
-        const formData = new FormData();
-                
+        const formData = new FormData();                
         this.loadingSubject.next(true);       
-
-        const formData = new FormData();
+     
         formData.append('csvFile', form.csvFile);
         formData.append('certificateTypeEnum', form.certificateTypeEnum);
-        formData.append('certificateModelId', form.certificateModelId.toString());
-        formData.append('csvFileDto', form.worksheet, form.worksheet.name);
+        formData.append('certificateModelId', form.certificateModelId.toString());   
         formData.append('eventName', form.eventName);
         formData.append('speakerName', form.speakerName);
         formData.append('speakerRole', form.speakerRole);
@@ -40,7 +32,7 @@ export class CertificateService{
         formData.append('eventLocale', form.eventLocale);
         
         return this.http.post<Certificate>(this.urlApi, formData, {
-            responseType: 'blob' as 'json',  // Indica que a resposta é um blob (arquivo)
+            responseType: 'blob' as 'json', 
         }).pipe(
             catchError( err => {
                 console.error(`Error na requisição: ${err}`);
@@ -48,18 +40,5 @@ export class CertificateService{
                 
             }),finalize(()=> this.loadingSubject.next(false))
         )
-    }
-
-    private getDateIssue():string {
-        const date = new Date();
-        const day = date.getDate();
-        const month = date.getMonth() + 1;
-        const year = date.getFullYear();
-        const dateIssue = `${year}-${this.addZero(month)}-${this.addZero(day)}`;
-        return dateIssue;
-    }
-
-    private addZero(value: number): string{
-        return value < 10 ? `0${value}` :`${value}`;
     }
 }
